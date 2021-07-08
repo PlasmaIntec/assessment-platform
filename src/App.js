7ea73d0ab54460aef4cd2723717cb5183be950f1
Matthew Ma <mtthman@amazon.com>
2021-07-07 23:48:46 -0700

    Revamp frontend with Bootstrap UI

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import ExampleTest from "./components/ExampleTest"
 import Login from "./components/Login"
 import useToken from './useToken';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -21,36 +21,31 @@ function App() {
 
   return (
     <Router>
-      <img src={logo} className="App-logo" alt="logo" />
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Assessment</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/users">Users</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <ExampleTest />
-          </Route>
-        </Switch>
-      </div>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <ExampleTest />
+        </Route>
+      </Switch>
     </Router>
   );
 }
--- src/components/ExampleTest.jsx
+++ src/components/ExampleTest.jsx
@@ -1,17 +1,52 @@
 import { test } from "../data/sampleTest"
+import { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
 
 export default function ExampleTest() {
+
+	const [responseList, setResponseList] = useState(Array(test.length))
+	const setSingleResponse = (idx, response) => {
+		var start = responseList.slice(0, idx)
+		start.push(response)
+		var end = responseList.slice(idx+1)
+		setResponseList(start.concat(end))
+	}
+
 	return (
 		<div>
 			<h2>Test</h2>
-			{test.map(testQuestion => (
-				<>
-					<h3>{testQuestion["question"]}</h3>
-					{testQuestion["options"].map(option => (
-						<h4>{option}</h4>
-					))}
-				</>
+			{test.map((testQuestion, idx) => (
+				<Card style={{ width: '18rem' }}>
+					<Card.Body>
+						<Card.Title>Question</Card.Title>
+						<Card.Text>
+							{testQuestion["question"]}
+						</Card.Text>
+					</Card.Body>
+					<TestQuestions idx={idx} setSingleResponse={setSingleResponse} response={responseList[idx]} options={testQuestion["options"]} />
+					<Card.Body>
+						<Card.Link href="#">Hint</Card.Link>
+						<Card.Link href="#">Answer</Card.Link>
+					</Card.Body>
+				</Card>
 			))}
 		</div>
+	)
+}
+
+function TestQuestions({ idx, setSingleResponse, response, options }) {
+
+	return (
+		<ListGroup className="list-group-flush">
+			{options.map(option => (
+				<ListGroupItem variant={option === response ? "success" : "light"} action onClick={() => {
+					setSingleResponse(idx, option)
+				}}>
+					{option}
+				</ListGroupItem>
+			))}
+		</ListGroup>
 	)
 }
